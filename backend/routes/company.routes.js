772f7eeff66b7260146a9b93794af87049bb0a1b
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2025-02-13 23:20:12 +0530

    Imported controllers in routes

--- backend/routes/company.routes.js
+++ backend/routes/company.routes.js
@@ -1,8 +1,8 @@
-import { companySignIn, companySignOut, companySignUp } from "../controllers/company.controllers.js";
+import { companySignIn, companySignOut, companySignUp, getSubmissions, updateSubmission } from "../controllers/company.controllers.js";
 import express from "express";
 import { Router } from "express";
 import authorizeCompany from "../middlewares/compnay.middlewares.js";
-import { createBounty, getAllBounties, getBountyById } from "../controllers/bounty.controllers.js";
+import { createBounty, deleteBountyById, getAllBounties, getBountyById, updateBountyById } from "../controllers/bounty.controllers.js";
 
 const companyRouter=Router();
 
@@ -15,4 +15,9 @@ companyRouter.post('/sign-out',companySignOut);
 companyRouter.post("/bounties",authorizeCompany,createBounty);
 companyRouter.get("/bounties",authorizeCompany,getAllBounties);
 companyRouter.get("/bounties/:id",authorizeCompany,getBountyById);
+companyRouter.put("/bounties/:id",authorizeCompany,updateBountyById);
+companyRouter.delete("/bounties/:id",authorizeCompany,deleteBountyById);
+companyRouter.get("/submissions",authorizeCompany,getSubmissions);
+companyRouter.patch("/submissions/:id",authorizeCompany,updateSubmission)
+
 export default companyRouter;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { Router } from "express";
 
-import { getUser, getUsers } from "../controllers/user.controllers.js";
+import { createSubmission, getSubmissions, getUser, getUsers } from "../controllers/user.controllers.js";
 import { authorize } from "../middlewares/auth.middlewares.js";
+import { getAllBounties } from "../controllers/bounty.controllers.js";
 
 const userRouter=Router();
 
 userRouter.get('/',getUsers);
+
+// protected route
+
 userRouter.get('/:id',authorize,getUser);
+userRouter.get('/bounties',authorize,getAllBounties);
+userRouter.post('/submit-bounty',authorize,createSubmission);
+userRouter.get('/get-submissions',authorize,getSubmissions);
 
 export default userRouter;
