06845817b4f5d1f4505c1ebe1db3759e716f97eb
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2025-02-13 23:18:28 +0530

    Added Create and Get Submission endpoint

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,3 +1,5 @@
+import Bounty from "../models/bounty.model.js";
+import Submission from "../models/submission.model.js";
 import User from "../models/user.model.js";
 
 export const getUsers=async(req,res,next)=>{
@@ -33,6 +35,60 @@ export const getUser=async(req,res,next)=>{
                 user
             }
         })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const createSubmission =async (req,res,next)=>{
+    try {
+        const { bounty , reportDetails , proof }= req.body;
+        const userId = req.user._id;
+
+        const bountyExist= await Bounty.findById(bounty);
+        if(!bountyExist){
+            const error = new Error ("Bounty not Found.")
+            error.statusCode=404
+            throw error
+        }
+        const newSubmission= await Submission.create({
+            bounty,
+            user:userId,
+            reportDetails,
+            proof
+        })
+        res
+        .status(200)
+        .json({
+            success:true,
+            message:"Created Submission Successfully",
+            data:{
+                Submission:newSubmission
+            }
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getSubmissions= async (req,res,next)=>{
+    try {
+        const userId = req.user._id;
+        
+        const submission= await Submission.find({
+            user:userId
+        })
+
+        res
+        .status(200)
+        .json({
+            success:true,
+            data:{
+                Submission: submission
+            }
+        })
+
     } catch (error) {
         next(error);
     }
